9a64827d2922686376b59e431eec68f87f5964e6
sohanmoz <amirhossainsohan0@gamil.com>
2021-07-25 16:59:47 +0600

    full project

--- js/app.js
+++ js/app.js
@@ -25,5 +25,12 @@ $(function() {
             +'<div class="col-md-3"><div class="count_down_item"><span>%H</span><span>Hours</span></div></div>'
             +'<div class="col-md-3"><div class="count_down_item"><span>%M</span><span>Mins</span></div></div>'
             +'<div class="col-md-3"><div class="count_down_item"><span>%S</span><span>Sec</span></div></div>'));
-    })
+    });
+
+    $('.testimonial_bg').slick({
+        prevArrow:'<i class="fas fa-chevron-left test_arrow left"></i>',
+        nextArrow:'<i class="fas fa-chevron-right test_arrow right"></i>',
+        autoplay: true,
+        autoplaySpeed: 3000,
+     });
 });
