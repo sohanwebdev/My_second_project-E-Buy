d89398f749e75945ea5958bee253042d3abc1400
sohanmoz <amirhossainsohan0@gmail.com>
2021-07-25 11:57:47 +0600

    e-buy full project

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(function() {
+    'use strict'
+    $('.slider_item_container').slick({
+       prevArrow:'<i class="fas fa-chevron-left slider_arrow left"></i>',
+       nextArrow:'<i class="fas fa-chevron-right slider_arrow right"></i>',
+       speed: 500,
+       fade: true,
+       cssEase: 'linear'
+    });
+
+    $('.featured_filter_container').filterizr();
+
+
+    $('.latest_product_section').slick({
+        prevArrow:'<i class="fas fa-chevron-left product_arrow left"></i>',
+        nextArrow:'<i class="fas fa-chevron-right product_arrow right"></i>',
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+    $('.count_down').countdown('2021/10/10', function (event) {
+        var $this = $(this).html(event.strftime(''
+            +'<div class="col-md-3"><div class="count_down_item"><span>%d</span><span>Days</span></div></div>'
+            +'<div class="col-md-3"><div class="count_down_item"><span>%H</span><span>Hours</span></div></div>'
+            +'<div class="col-md-3"><div class="count_down_item"><span>%M</span><span>Mins</span></div></div>'
+            +'<div class="col-md-3"><div class="count_down_item"><span>%S</span><span>Sec</span></div></div>'));
+    })
+});
