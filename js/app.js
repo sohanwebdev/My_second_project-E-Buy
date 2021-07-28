d7de1431dcbbcd1c5747a5522c8b355690038cc6
sohanmoz <amirhossainsohan0@gamil.com>
2021-07-28 22:33:27 +0600

    pesponsive

--- js/app.js
+++ js/app.js
@@ -17,14 +17,22 @@ $(function() {
         slidesToShow: 4,
         autoplay: true,
         autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 2,
+                }
+            },
+        ]
     });
 
     $('.count_down').countdown('2021/10/10', function (event) {
         var $this = $(this).html(event.strftime(''
-            +'<div class="col-md-3"><div class="count_down_item"><span>%d</span><span>Days</span></div></div>'
-            +'<div class="col-md-3"><div class="count_down_item"><span>%H</span><span>Hours</span></div></div>'
-            +'<div class="col-md-3"><div class="count_down_item"><span>%M</span><span>Mins</span></div></div>'
-            +'<div class="col-md-3"><div class="count_down_item"><span>%S</span><span>Sec</span></div></div>'));
+            +'<div class="col-3"><div class="count_down_item"><span>%d</span><span>Days</span></div></div>'
+            +'<div class="col-3"><div class="count_down_item"><span>%H</span><span>Hours</span></div></div>'
+            +'<div class="col-3"><div class="count_down_item"><span>%M</span><span>Mins</span></div></div>'
+            +'<div class="col-3"><div class="count_down_item"><span>%S</span><span>Sec</span></div></div>'));
     });
 
     $('.testimonial_bg').slick({
